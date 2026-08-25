a40484c23bdcc7ad96c040344c7e0b2b37bcb556
Jimmy Pautz <jpautz@watermarkinsights.com>
2026-08-25 13:16:32 -0500

    Update test

--- spec/mocks/xray/series/the_wheel_of_time.lua
+++ spec/mocks/xray/series/the_wheel_of_time.lua
@@ -2,20 +2,20 @@
 return {
   books = {
     [1] = {
-      characters = {
-        [1] = {
-          description = "Survivor of Hathsin",
-          name = "Kelsier",
-        },
-      },
       locations = {
         [1] = {
-          description = "Capital city",
           name = "Luthadel",
+          description = "Capital city",
+        },
+      },
+      characters = {
+        [1] = {
+          name = "Kelsier",
+          description = "Survivor of Hathsin",
         },
       },
     },
   },
-  cached_at = 1787674800,
+  cached_at = 1787681577,
   cache_version = "6.0",
 }
